7bde4495a26ee049fa18154e2449e739962d0974
Joe Dodd <joehdodd@gmail.com>
2023-07-07 18:49:04 -0400

    Mailto

--- src/components/Info/index.js
+++ src/components/Info/index.js
@@ -6,14 +6,14 @@ const Info = () => {
     <div>
       <h2 className="info-headline">Hello!</h2>
       <p className="info-headline-text">
-        We're glad that you found us! We love capturing those intimate
-        details of your wedding day that will remind you of how special it
-        really was.
+        We're glad that you found us! We love capturing those intimate details
+        of your wedding day that will remind you of how special it really was.
       </p>
       <p className="info-text">
         Interested in choosing us as your Chattanoog wedding photographers? Send
-        an email to info@joeandselah.co for information on packages and prices.
-        Our packages currently start at $1,600.
+        an email to <a href="mailto:info@joeandselah.co">info@joeandselah.co</a>{" "}
+        for information on packages and prices. Our packages currently start at
+        $1,600.
       </p>
     </div>
   );
