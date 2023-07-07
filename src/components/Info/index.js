76061d554b6deb1064ce163176229f4a31c30eda
Joe Dodd <joehdodd@gmail.com>
2023-07-07 19:05:49 -0400

    Update styles.

--- src/components/Info/index.js
+++ src/components/Info/index.js
@@ -5,9 +5,13 @@ const Info = () => {
   return (
     <div>
       <h2 className="info-headline">Hello!</h2>
-      <p className="info-headline-text">
-        We're glad that you found us! We love capturing those intimate details
-        of your wedding day that will remind you of how special it really was.
+      <p className="info-headline-text">We're glad that you found us!</p>
+      <hr/>
+      <p className="info-text">
+        We are Chattanooga wedding photographers specializing in intimate,
+        documentary style photography for engagements and weddings. We love
+        capturing those intimate details of your wedding day that will remind
+        you of how special it really was.
       </p>
       <p className="info-text">
         Interested in choosing us as your Chattanoog wedding photographers? Send
