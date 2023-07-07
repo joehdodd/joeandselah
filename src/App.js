9d52fd96fbe896f94bc7d85b6f4b45e6e09b59b9
Joe Dodd <joehdodd@gmail.com>
2023-07-07 14:42:59 -0400

    Updates.

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import ImageGrid from "./components/ImageGrid";
 import "./App.css";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
+import Info from "./components/Info";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Hero />
       <div className="app-wrapper">
         <ImageGrid />
+        <Info />
       </div>
     </>
   );
---
+++ src/components/Info/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./Info.css";
+
+const Info = () => {
+  return (
+    <div>
+      <p className="info-text">
+        Interested in choosing us as your Chattanoog wedding photographers? Send
+        an email to info@joeandselah.co for information on packages and prices.
+        Our packages currently start at $1,000.
+      </p>
+    </div>
+  );
+};
+
+export default Info;
